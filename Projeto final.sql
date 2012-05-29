bdbef6640c9b0ec0801544e423d956d2d05ad79e
Marcia Fatima <marciafatima93@hotmail.com>
2012-05-28 21:58:59 -0300

    Adicionei o banco de dados

---
+++ Projeto final.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE SistemaFinanceiro;
+GO
+
+USE SistemaFinanceiro;
+GO
+
+CREATE TABLE Categoria(
+Codigo INT  NOT NULL PRIMARY KEY IDENTITY,
+Descrição VARCHAR(20) NOT NULL,
+Orcamento DECIMAL(6,2) NOT NULL);
+GO
+
+CREATE TABLE Registros(
+Codigo INT NOT NULL PRIMARY KEY IDENTITY,
+Descricao VARCHAR(40) NOT NULL ,
+Valor DECIMAL(6,2) NOT NULL ,
+Categoria INT  NOT NULL FOREIGN KEY REFERENCES Categoria(Codigo),
+Status1 int,
+DataVencimento DATE NOT NULL ,
+DataPagamneto DATE,
+DataCadastro DATE,
+Parcelas int);
+GO
+
+INSERT INTO Categoria(Descrição, Orcamento) VALUES('Casa', 200.00);
+INSERT INTO Categoria(Descrição, Orcamento) VALUES('Carro', 350.00);
+INSERT INTO Categoria(Descrição, Orcamento) VALUES('Outros', 150.00);
+
+INSERT INTO Registros(Categoria, Descricao, Valor, DataCadastro, DataVencimento, DataPagamneto, Parcelas, Status1) VALUES(1, 'Água', -35.50, getdate(),'05-23-2012', '06-09-2012', null, 1);
+INSERT INTO Registros(Categoria, Descricao, Valor, DataCadastro, DataVencimento, DataPagamneto, Parcelas, Status1) VALUES(1, 'Lúz', -75.00, getdate(), '05-13-2012', '06-08-2012', null, 1);
+INSERT INTO Registros(Categoria, Descricao, Valor, DataCadastro, DataVencimento, DataPagamneto, Parcelas, Status1) VALUES(3, 'Outros', -35.50, getdate(), '05-17-2012', '06-15-2012',  null, 1);
+INSERT INTO Registros(Categoria, Descricao, Valor, DataCadastro, DataVencimento, DataPagamneto, Parcelas, Status1) VALUES(2, 'Gasolina', -60.50, getdate(), '05-22-2012', '06-18-2012',  null, 1);
+INSERT INTO Registros(Categoria, Descricao, Valor, DataCadastro, DataVencimento, DataPagamneto, Parcelas, Status1) VALUES(3, 'Remédio', -35.50, getdate(), '05-20-2012', '06-25-2012', null, 1);
+
